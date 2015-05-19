989bbaeb9311e2c1ffe8141ffeb847766e565f70
argotechnica <corysalveson@gmail.com>
2015-05-18 22:51:04 -0500

    more lessons and problem sets!
    made it through lesson & problem set 5

--- 3.3_dive-into-eda/lesson_5.R
+++ 3.3_dive-into-eda/lesson_5.R
@@ -1,253 +1,150 @@
-Lesson 5
-========================================================
+# Write code to create a new data frame,
+# called 'pf.fc_by_age_gender', that contains
+# information on each age AND gender group.
 
-### Multivariate Data
-Notes:
+# The data frame should contain the following variables:
 
-***
+#    mean_friend_count,
+#    median_friend_count,
+#    n (the number of users in each age and gender grouping)
 
-### Moira Perceived Audience Size Colored by Age
-Notes:
+# Here is an example of the structure of your data frame. Your
+# data values will be different. Note that if you are grouping by
+# more than one variable, you will probably need to call the
+# ungroup() function. 
 
-***
+#   age gender mean_friend_count median_friend_count    n
+# 1  13 female          247.2953                 150  207
+# 2  13   male          184.2342                  61  265
+# 3  14 female          329.1938                 245  834
+# 4  14   male          157.1204                  88 1201
 
-### Third Qualitative Variable
-Notes:
+# See the Instructor Note for two hints.
 
-```{r Third Qualitative Variable}
-ggplot(aes(x = gender, y = age),
-       data = subset(pf, !is.na(gender))) + geom_histogram()
-```
+# DO NOT DELETE THESE NEXT TWO LINES OF CODE
+# ==============================================================
+pf <- read.delim('/datasets/ud651/pseudo_facebook.tsv')
+suppressMessages(library(dplyr))
 
-***
+# ENTER YOUR CODE BELOW THIS LINE.
+# ==============================================================
+suppressMessages(library(ggplot2))
+suppressMessages(library(GGally))
+suppressMessages(library(dplyr))
 
-### Plotting Conditional Summaries
-Notes:
+setwd("C:/Client/GitHub/sliderule-workshop/3.3_dive-into-eda")
+pf <- read.csv('pseudo_facebook.tsv', sep = '\t')
+  
+#example from lesson 4
+pf.fc_by_age <- pf %>%
+  group_by(age) %>%
+  summarise(friend_count_mean = mean(friend_count),
+            friend_count_median = median(friend_count),
+            n = n()) %>%
+  arrange(age)
 
-```{r Plotting Conditional Summaries}
+#new stuff for lesson 5... group by multiple
+pf.fc_by_age_gender <- pf %>%
+  filter(!is.na(gender)) %>%
+  group_by(age,gender) %>%
+  summarise(mean_friend_count = mean(friend_count),
+            median_friend_count = median(as.numeric(friend_count)),
+            n = n()) %>%
+  ungroup() %>%
+  arrange(age)
 
-```
+names(pf.fc_by_age_gender)
 
-***
+ggplot(pf.fc_by_age_gender, aes(age,median_friend_count)) +
+  geom_line(aes(color = gender))
 
-### Thinking in Ratios
-Notes:
+#look at reshaping data
+#library(reshape2) instead of using reshape2, use tidyr
+library(tidyr)
+library(dplyr)
 
-***
+pf.fc_by_age_gender.wide <- pf.fc_by_age_gender[c('age', 'gender', 'median_friend_count')] %>%
+  filter(!is.na(gender)) %>%
+  spread(gender, median_friend_count) %>%
+  mutate(ratio = male / female)
+head(pf.fc_by_age_gender.wide)
 
-### Wide and Long Format
-Notes:
+ggplot(pf.fc_by_age_gender.wide, aes(age,female / male)) +
+  geom_line() +
+  geom_hline(yintercept = 1, linetype = 2, alpha = 0.5)
 
-***
+#use cut to create a new numerical variable from an existing one, then plot it
+pf$year_joined <- 2014-(pf$tenure/365.25)
+pf$year_joined.bucket <- cut(pf$year_joined,
+                             c(2004, 2009, 2011, 2012, 2014))
 
-### Reshaping Data
-Notes:
+ggplot(subset(pf, !is.na(year_joined.bucket)), aes(age, friend_count)) +
+  geom_line(aes(color = year_joined.bucket),
+            stat = 'summary',
+            fun.y = median)
 
-```{r}
-install.packages('reshape2')
-library(reshape2)
-```
+ggplot(subset(pf, !is.na(year_joined.bucket)), aes(age, friend_count)) +
+  geom_line(aes(color = year_joined.bucket),
+            stat = 'summary',
+            fun.y = mean) +
+  geom_line(linetype = 2,
+            stat = 'summary',
+            fun.y= mean)
 
+# look at friending rate - v1, I do it with a var
+friending_rate <- subset(pf$friend_count/pf$tenure, pf$tenure >= 1)
+median(friending_rate)
+max(friending_rate)
 
-***
+# look at friending rate - v2, tut does it with "with"
+with(subset(pf, tenure >=1), summary(friend_count/tenure))
 
-### Ratio Plot
-Notes:
-
-```{r Ratio Plot}
-
-```
-
-***
-
-### Third Quantitative Variable
-Notes:
-
-```{r Third Quantitative Variable}
-
-```
-
-***
-
-### Cut a Variable
-Notes:
-
-```{r Cut a Variable}
-
-```
-
-***
-
-### Plotting it All Together
-Notes:
-
-```{r Plotting it All Together}
-
-```
-
-***
-
-### Plot the Grand Mean
-Notes:
-
-```{r Plot the Grand Mean}
-
-```
-
-***
-
-### Friending Rate
-Notes:
-
-```{r Friending Rate}
-
-```
-
-***
-
-### Friendships Initiated
-Notes:
-
-What is the median friend rate?
-
-What is the maximum friend rate?
-
-```{r Friendships Initiated}
-
-```
-
-***
-
-### Bias-Variance Tradeoff Revisited
-Notes:
-
-```{r Bias-Variance Tradeoff Revisited}
-
-ggplot(aes(x = tenure, y = friendships_initiated / tenure),
-       data = subset(pf, tenure >= 1)) +
+ggplot(subset(pf, tenure>=1), aes(tenure, friendships_initiated/tenure)) +
   geom_line(aes(color = year_joined.bucket),
             stat = 'summary',
             fun.y = mean)
 
 ggplot(aes(x = 7 * round(tenure / 7), y = friendships_initiated / tenure),
-       data = subset(pf, tenure > 0)) +
-  geom_line(aes(color = year_joined.bucket),
-            stat = "summary",
-            fun.y = mean)
+       data = subset(pf, tenure >= 1)) +
+  geom_smooth(aes(color = year_joined.bucket))                                                    
 
-ggplot(aes(x = 30 * round(tenure / 30), y = friendships_initiated / tenure),
-       data = subset(pf, tenure > 0)) +
-  geom_line(aes(color = year_joined.bucket),
-            stat = "summary",
-            fun.y = mean)
+# and now for something completely different... :)
+suppressMessages(library(ggplot2))
+suppressMessages(library(GGally))
+suppressMessages(library(dplyr))
 
-ggplot(aes(x = 90 * round(tenure / 90), y = friendships_initiated / tenure),
-       data = subset(pf, tenure > 0)) +
-  geom_line(aes(color = year_joined.bucket),
-            stat = "summary",
-            fun.y = mean)
+setwd("C:/Client/GitHub/sliderule-workshop/3.3_dive-into-eda")
 
-```
+yo <- read.csv('yogurt.csv')
 
-***
+yo$id <- factor(yo$id)
+str(yo)
+summary(yo)
 
-### Sean's NFL Fan Sentiment Study
-Notes:
+ggplot(yo, aes(x=price)) + geom_histogram()
 
-***
+# use transform to create a summed column
+yo <- transform(yo,all.purchases = strawberry + blueberry + pina.colada + plain + mixed.berry)
 
-### Introducing the Yogurt Data Set
-Notes:
+#scatterplot the data
+ggplot(yo, aes(time,price)) +
+  geom_jitter(alpha = 1/4, shape = 21)
 
-***
+#use a set seed to sample and then plo the data
+#to get a feel for 16 specific households
+set.seed(4320)
+sample.ids <- sample(levels(yo$id), 16)
 
-### Histograms Revisited
-Notes:
+ggplot(aes(x = time, y = price),
+       data = subset(yo, id %in% sample.ids)) +
+  facet_wrap( ~ id) +
+  geom_line() +
+  geom_point(aes(size = all.purchases), pch = 1)
 
-```{r Histograms Revisited}
+#now use sampling and GGally to look at the pf dataset again
+suppressMessages(library(GGally))
 
-```
-
-***
-
-### Number of Purchases
-Notes:
-
-```{r Number of Purchases}
-
-```
-
-***
-
-### Prices over Time
-Notes:
-
-```{r Prices over Time}
-
-```
-
-***
-
-### Sampling Observations
-Notes:
-
-***
-
-### Looking at Samples of Households
-
-```{r Looking at Sample of Households}
-
-```
-
-***
-
-### The Limits of Cross Sectional Data
-Notes:
-
-***
-
-### Many Variables
-Notes:
-
-***
-
-### Scatterplot Matrix
-Notes:
-
-***
-
-### Even More Variables
-Notes:
-
-***
-
-### Heat Maps
-Notes:
-
-```{r}
-nci <- read.table("nci.tsv")
-colnames(nci) <- c(1:64)
-```
-
-```{r}
-nci.long.samp <- melt(as.matrix(nci[1:200,]))
-names(nci.long.samp) <- c("gene", "case", "value")
-head(nci.long.samp)
-
-ggplot(aes(y = gene, x = case, fill = value),
-  data = nci.long.samp) +
-  geom_tile() +
-  scale_fill_gradientn(colours = colorRampPalette(c("blue", "red"))(100))
-```
-
-
-***
-
-### Analyzing Three of More Variables
-Reflection:
-
-***
-
-Click **KnitHTML** to see all of your hard work and to have an html
-page of this lesson, your answers, and your notes!
-
+theme_set(theme_minimal(20))
+pf_subset <- pf[, c(2:15)]
+names(pf_subset)
+ggpairs(pf_subset[sample.int(nrow(pf_subset), 1000), ])
--- 3.3_dive-into-eda/problem_set_5.R
+++ 3.3_dive-into-eda/problem_set_5.R
@@ -1,3 +1,6 @@
+library(ggplot2)
+str(diamonds)
+
 # Create a histogram of diamond prices.
 # Facet the histogram by diamond color
 # and use cut to color the histogram bars.
@@ -14,3 +17,169 @@
 
 # ENTER YOUR CODE BELOW THIS LINE
 # ===========================================
+ggplot(aes(x = price, fill = cut),
+       data = diamonds) +
+  geom_histogram() +
+  scale_fill_brewer(type = 'qual') +
+  facet_wrap( ~ color)
+
+# Create a scatterplot of diamond price vs.
+# table and color the points by the cut of
+# the diamond.
+
+# The plot should look something like this.
+# http://i.imgur.com/rQF9jQr.jpg
+
+# Note: In the link, a color palette of type
+# 'qual' was used to color the scatterplot using
+# scale_color_brewer(type = 'qual')
+
+# This assignment is not graded and
+# will be marked as correct when you submit.
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ===========================================
+ggplot(aes(x = table, y = price, color = cut),
+       data = diamonds) +
+  geom_point() +
+  scale_color_brewer(type = 'qual')
+
+# Create a scatterplot of diamond price vs.
+# volume (x * y * z) and color the points by
+# the clarity of diamonds. Use scale on the y-axis
+# to take the log10 of price. You should also
+# omit the top 1% of diamond volumes from the plot.
+
+# Note: Volume is a very rough approximation of
+# a diamond's actual volume.
+
+# The plot should look something like this.
+# http://i.imgur.com/excUpea.jpg
+
+# Note: In the link, a color palette of type
+# 'div' was used to color the scatterplot using
+# scale_color_brewer(type = 'div')
+
+# This assignment is not graded and
+# will be marked as correct when you submit.
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ===========================================
+ggplot(aes(x = (x*y*z), y = price, color = clarity),
+       data = diamonds) +
+  geom_point() +
+  scale_y_log10() +
+  scale_color_brewer(type = 'div') +
+  xlim(0, quantile(diamonds$x * diamonds$y * diamonds$z, 0.99)) +
+  xlab("volume")
+
+# Many interesting variables are derived from two or more others.
+# For example, we might wonder how much of a person's network on
+# a service like Facebook the user actively initiated. Two users
+# with the same degree (or number of friends) might be very
+# different if one initiated most of those connections on the
+# service, while the other initiated very few. So it could be
+# useful to consider this proportion of existing friendships that
+# the user initiated. This might be a good predictor of how active
+# a user is compared with their peers, or other traits, such as
+# personality (i.e., is this person an extrovert?).
+
+# Your task is to create a new variable called 'prop_initiated'
+# in the Pseudo-Facebook data set. The variable should contain
+# the proportion of friendships that the user initiated.
+
+# This programming assignment WILL BE automatically graded.
+
+# DO NOT DELETE THIS NEXT LINE OF CODE
+# ========================================================================
+pf <- read.delim('/datasets/ud651/pseudo_facebook.tsv')
+
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ========================================================================
+#setwd("C:/Client/GitHub/sliderule-workshop/3.3_dive-into-eda")
+#pf <- read.csv('pseudo_facebook.tsv', sep = '\t')
+
+pf$prop_initiated <- ifelse(pf$friend_count > 0, pf$friendships_initiated / pf$friend_count, 0)
+
+# Create a line graph of the median proportion of
+# friendships initiated ('prop_initiated') vs.
+# tenure and color the line segment by
+# year_joined.bucket.
+
+# Recall, we created year_joined.bucket in Lesson 5
+# by first creating year_joined from the variable tenure.
+# Then, we used the cut function on year_joined to create
+# four bins or cohorts of users.
+
+# (2004, 2009]
+# (2009, 2011]
+# (2011, 2012]
+# (2012, 2014]
+
+# The plot should look something like this.
+# http://i.imgur.com/vNjPtDh.jpg
+# OR this
+# http://i.imgur.com/IBN1ufQ.jpg
+
+# This assignment is not graded and
+# will be marked as correct when you submit.
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ===========================================================
+pf$year_joined <- 2014-(pf$tenure/365)
+pf$year_joined.bucket <- cut(pf$year_joined,
+                             c(2004, 2009, 2011, 2012, 2014))
+
+ggplot(aes(x = tenure, y = prop_initiated),
+       data = subset(pf, !is.na(year_joined.bucket))) +
+  geom_line(aes(color = year_joined.bucket),
+            stat = 'summary',
+            fun.y = median)
+
+# Smooth the last plot you created of
+# of prop_initiated vs tenure colored by
+# year_joined.bucket. You can use larger
+# bins for tenure or add a smoother to the plot.
+
+# There won't be a solution image for this exercise.
+# You will answer some questions about your plot in
+# the next two exercises.
+
+# This assignment is not graded and
+# will be marked as correct when you submit.
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ====================================================
+ggplot(aes(x = tenure, y = prop_initiated),
+       data = subset(pf, !is.na(year_joined.bucket))) +
+  geom_smooth(aes(color = year_joined.bucket),
+            stat = 'summary',
+            fun.y = median)
+
+by(pf$prop_initiated, pf$year_joined.bucket, summary)
+
+summary(pf$prop_initiated[pf$year_joined.bucket == "(2012,2014]"])
+
+# Create a scatter plot of the price/carat ratio
+# of diamonds. The variable x should be
+# assigned to cut. The points should be colored
+# by diamond color, and the plot should be
+# faceted by clarity.
+
+# The plot should look something like this.
+# http://i.imgur.com/YzbWkHT.jpg.
+
+# Note: In the link, a color palette of type
+# 'div' was used to color the histogram using
+# scale_color_brewer(type = 'div')
+
+# This assignment is not graded and
+# will be marked as correct when you submit.
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ===========================================
+ggplot(aes(x = cut, y = price, color = color),
+       data = diamonds) +
+  geom_jitter() +
+  facet_wrap( ~ clarity)
